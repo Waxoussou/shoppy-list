7dc9d92daca5d3f05e9f7b42dce1ec932b6281d2
wax <maxime.abehserra@gmail.com>
2021-06-18 12:42:17 +0200

    adding login.vue file

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <form class="login-form">
+            <input type="text" placeholder="username" id="username-login" />
+            <input type="password" placeholder="password" id="password-login" />
+        </form>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
